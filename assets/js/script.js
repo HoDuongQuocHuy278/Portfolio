3d4e3fdd3aad286e5f2e0c61007fc5daeac051f7
Ho Duong Quoc Huy <HoDuongQuocHuy278@users.noreply.github.com>
2026-01-22 07:39:06 +0700

    update profile moi

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Van Viet";
+            document.title = "Portfolio | Quoc Huy";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
